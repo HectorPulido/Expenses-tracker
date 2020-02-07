523f591df5175ef416bd7679621bf5136b7572a9
root <root@localhost.localdomain>
2020-02-06 22:39:26 -0500

    flash notifications, data saved, orders

--- const.py
+++ const.py
@@ -2,3 +2,5 @@ import os
 
 CERTIFICATE = "expensestracker3-firebase-adminsdk-dkc6m-7ff1003922.json"
 SECRET_FLASK_KEY = "expensesTracker00012354648451321-7ff1003922"
+PORT = 5000
+DEBUG = True
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from flask import jsonify
-from flask import Flask, session, redirect, url_for, request, render_template
+from flask import Flask, session, redirect, url_for, request, render_template, flash
 from firebase_admin import credentials, firestore
 from Class.Expense import Expense
 from Class.User import User
@@ -10,70 +10,75 @@ app = Flask(__name__)
 app.secret_key = SECRET_FLASK_KEY
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    user_id = session.get('user_id', None)
+    user_id = session.get("user_id", None)
     if user_id is None:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
     expenses = Expense.getExpenses(user_id)
-    return render_template('expenses.html', expenses=expenses)
+    return render_template("expenses.html", expenses=expenses)
 
-
-@app.route('/register_data', methods=['POST', 'GET'])
-def register_data():
-    user_id = session.get('user_id', None)
+@app.route("/seeall")
+def seeall():
+    user_id = session.get("user_id", None)
     if user_id is None:
-        return redirect(url_for('login'))
-    if request.method != 'POST':
-        return redirect(url_for('index'))
+        return redirect(url_for("login"))
+    expenses = Expense.getExpenses(user_id, None)
+    return render_template("expenses.html", expenses=expenses)
 
-    type_of_register = request.form.get("type_of_register", None)
+@app.route("/register_data", methods=["POST", "GET"])
+def register_data():
+    user_id = session.get("user_id", None)
+    if user_id is None:
+        return redirect(url_for("login"))
+    if request.method != "POST":
+        return redirect(url_for("index"))
+
+    type_data = request.form.get("type_data", None)
     value = request.form.get("value", None)
     description = request.form.get("description", None)
 
-    if type_of_register is None:
-        return redirect(url_for('index'))
+    if type_data is None:
+        flash("type data is necessary to continue")
+        return redirect(url_for("index"))
     if value is None:
-        return redirect(url_for('index'))
+        flash("value is necessary to continue")
+        return redirect(url_for("index"))
     if description is None:
-        return redirect(url_for('index'))
+        flash("descriptionis necessary to continue")
+        return redirect(url_for("index"))
 
-    Expense()
+    exp = Expense(description, value, type_data, user_id)
+    exp.saveExpense()
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-
-@app.route('/seeall')
-def seeall():
-    user_id = session.get('user_id', None)
-    if user_id is None:
-        return redirect(url_for('login'))
-    return redirect(url_for('index'))
-
-
-@app.route('/login', methods=['POST', 'GET'])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
         password = request.form.get("password", None)
         username = request.form.get("username", None)
 
         if password is None:
-            return render_template('login.html', error="Password missing")
+            flash("Password missing")
+            return render_template("login.html")
 
         if username is None:
-            return render_template('login.html', error="Username missing")
+            flash("Username missing")
+            return render_template("login.html")
 
         val, id = User.validate_user(username, password)
         if val:
             session["user_id"] = id
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-        return render_template('login.html', error="Username or Password invalid")
+        flash("Username or Password invalid")
+        return render_template("login.html")
 
-    return render_template('login.html')
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
     cred = credentials.Certificate(CERTIFICATE)
     firebase_admin.initialize_app(cred)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
