32d303a4271d65263422b29624e2473f082835a0
huellitasoga <huellasyantecedentes@gmail.com>
2025-07-18 13:28:47 -0300

    Update settings.py

--- src/expenses_tracker/settings.py
+++ src/expenses_tracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "thisisnotasecretkey")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "coolify.local",
+    "ubuntu.local:8001",
     os.environ.get("ALLOWED_HOST", "allowed_host"),
 ]
 
